8ae86bc27a6482763cef1078b526d772911d429e
mahmoudAdham96 <88621466+mahmoudAdham96@users.noreply.github.com>
2021-09-03 06:22:47 +0200

    commt tradex

--- js/script.js
+++ js/script.js
@@ -37,3 +37,62 @@ window.addEventListener('scroll',function(){
     topbtn.style.display ='block'
     else topbtn.style.display ='none'
 })
+
+// --------------------------------- valadtion--------------------------
+var form = document.getElementById('myForm')
+    myButton = document.getElementById('myButton')
+form.addEventListener('input' , function(e){
+    switch(e.target.id){
+        case 'username' : 
+            checkName(e.target)
+            break
+        case 'email' :
+            checkEmail(e.target)
+            break
+        case 'password' : 
+        checkPassword(e.target) 
+        break
+    }
+})
+myButton.addEventListener("click" , function(e){
+    e.preventDefault()
+    var name=document.getElementById('username')
+        user=  name.value.trim()
+    if (user==null || user=="") showError(name , "Name can't be blank")
+    var x=document.getElementById('email')
+        email=  x.value.trim()
+    if (email==null || email=="") showError(x , "Email can't be blank")
+    var y=document.getElementById('password')
+    password=  y.value.trim()
+    if (password==null || password=="") showError(y , "password can't be blank")
+    
+    
+})
+function checkName(inp){
+    console.log(inp.value)
+    var inputValue = inp.value.trim()
+    if(inputValue.length >= 8) removeError(inp)
+    else showError(inp , 'username must be at least 8 character' )
+}
+
+function checkEmail(inp){
+    var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    var inputValue = inp.value
+    console.log(emailReg.test(inputValue))
+    if(emailReg.test(inputValue) == false) showError(inp , 'email not valid')
+    else removeError(inp) 
+}
+ function checkPassword (inp){
+    var passReg = /^[A-Za-z]\w{7,14}$/
+     var passvalue = inp.value
+     if(passReg.test(passvalue) == false ) showError(inp,'Input Password and Submit [7 to 15 characters which contain only characters, numeric digits, underscore and first character must be a letter')
+     else removeError(inp, '') 
+ }
+
+function showError(element , msg){
+    element.nextElementSibling.innerText = msg
+}
+
+function removeError(element){
+    element.nextElementSibling.innerText = ''
+}
